65a460f906f18307c24e151726884a1bd5c3e5b1
Nitrage <44818269+Nitrage@users.noreply.github.com>
2018-11-07 06:46:31 +0100

    Add files via upload

---
+++ reset.bat
@@ -0,0 +1,6 @@
+rem Local\Hikari06\Patched.exe_Url_*\1.0.0.0\user.config
+rmdir /S /Q "%appdata%\..\Local\Hikari06"
+rmdir /S /Q "%appdata%\..\Local\IsolatedStorage"
+rmdir /S /Q "%appdata%\USB_HELPER"
+
+pause
