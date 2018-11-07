41f8d96b5c15d13da769f34e5195cf33374e4b44
Nitrage <44818269+Nitrage@users.noreply.github.com>
2018-11-07 06:48:42 +0100

    Update reset.bat

--- reset.bat
+++ reset.bat
@@ -1,6 +1,5 @@
 rem Local\Hikari06\Patched.exe_Url_*\1.0.0.0\user.config
-rmdir /S /Q "%appdata%\..\Local\Hikari06"
-rmdir /S /Q "%appdata%\..\Local\IsolatedStorage"
+rmdir /S /Q "%localappdata%\Hikari06"
+rmdir /S /Q "%localappdata%\IsolatedStorage"
 rmdir /S /Q "%appdata%\USB_HELPER"
-
 pause
